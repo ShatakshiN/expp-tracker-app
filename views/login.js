f10e7db032339f9aaf501e6f0b349d59a8887bfc
ShatakshiN <shatakshinimare27@gmail.com>
2024-03-18 18:57:26 +0530

    expense tracker

---
+++ views/login.js
@@ -0,0 +1,60 @@
+async function sendData(event){
+    event.preventDefault();
+
+    const email  = event.target.mail.value;
+    const password = event.target.password.value;
+    obj = {
+        email,
+        password
+    }
+
+    try{
+        const response  = await axios.post('http://localhost:4000/login',obj);      
+    }
+    catch(error){
+        console.log(error)
+        showAlert(error.response.data.msg);    
+    }
+}
+
+function showAlert(str){
+    if(str ==="password incorrect"){
+        const parentElem = document.getElementById('loginForm');
+        const childElem = document.createElement('div');
+        childElem.className = 'alert alert-danger';
+        childElem.role = 'alert';
+        childElem.textContent = 'incorrect Password! please try again';
+        const okButton = document.createElement('button');
+        okButton.className = 'btn btn-dark';
+        okButton.textContent = 'OK';
+        // Add click event listener to the OK button
+        okButton.addEventListener('click', function() {
+            parentElem.removeChild(childElem); // Remove the alert from the DOM
+        });
+        
+        childElem.appendChild(okButton);
+        //parentElem.appendChild(childElem);
+        parentElem.insertBefore(childElem, parentElem.firstChild);
+
+    }
+    if(str=== "user doesn't exist"){
+        const parentElem = document.getElementById('loginForm');
+        const childElem = document.createElement('div');
+        childElem.className = 'alert alert-danger';
+        childElem.role = 'alert';
+        childElem.textContent = "user Doesn't exists!";
+        const okButton = document.createElement('button');
+        okButton.className = 'btn btn-dark';
+        okButton.textContent = 'OK';
+        // Add click event listener to the OK button
+        okButton.addEventListener('click', function() {
+            parentElem.removeChild(childElem); // Remove the alert from the DOM
+        });
+        
+        childElem.appendChild(okButton);
+        //parentElem.appendChild(childElem);
+        parentElem.insertBefore(childElem, parentElem.firstChild);
+
+    }
+    
+};
---
+++ views/signUp.js
@@ -0,0 +1,23 @@
+async function sendUserData(event){
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.mail.value; 
+    const password = event.target.password.value;
+    
+    obj = {
+        name,
+        email,
+        password
+    }
+
+    try{
+        const response = await axios.post('http://localhost:4000/signUp',obj)
+        /* if(response.data.includes('user already exists !')){
+            alert("user already exists !")
+        } */
+
+    }catch(error){
+        console.log(error)
+        
+    }
+};

ae725f2af428c032aa030168956887e12cece6a9
ShatakshiN <shatakshinimare27@gmail.com>
2024-03-22 17:44:07 +0530

    added id

---
+++ views/daily.js
@@ -0,0 +1,27 @@
+async function sendData(event){
+    event.preventDefault();
+    const date  = event.target.date.value;
+    const description = event.target.description.value;
+    const amount = event.target.amount.value;
+    const category = event.target.categories.value;
+
+    obj = {
+        date,
+        description,
+        amount,
+        category
+    }
+
+    try{
+        const response = axios.post('http://localhost:4000/daily-expense',obj);
+        showExpenseOnScreen(response.data.expenseData)
+
+    }catch(error){
+        console.log(error);
+    }
+
+};
+
+function showExpenseOnScreen(obj){
+
+}

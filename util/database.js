c83bd5a46180c86fa49867a3663063b7476edbca
ShatakshiN <shatakshinimare27@gmail.com>
2024-06-28 03:42:10 +0530

    deployment ready

--- util/database.js
+++ util/database.js
@@ -20,6 +20,6 @@ the MySQL database.
 require('dotenv').config();
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(process.env.DBASE_NAME,process.env.DBASE_USERNAME, process.env.DBASE_PASSWORD ,{dialect : 'mysql', host : 'localhost'}) // instance of Sequelize
+const sequelize = new Sequelize(process.env.DBASE_NAME,process.env.DBASE_USERNAME, process.env.DBASE_PASSWORD ,{dialect : 'mysql', host :process.env.DBASE_HOST}) // instance of Sequelize
 
 module.exports= sequelize;

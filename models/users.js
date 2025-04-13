82282f07579d102c10e1e42ef8aa1c1014bb8ed2
ShatakshiN <147754096+ShatakshiN@users.noreply.github.com>
2025-04-13 19:32:06 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,35 +1,31 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database')
+const mongoose  = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Users  = sequelize.define('SignUp',{
-    id : {
-        type : Sequelize.INTEGER,
-        allowNull : false,
-        primaryKey : true,
-        autoIncrement : true
+const userSchema  = new Schema({
+    name:{
+        type : String,
+        required: true
     },
-
-    name : {
-        type : Sequelize.STRING,
-        allowNull : false  
+    email:{
+        type: String,
+        required: true,
+        unique: true 
     },
-
-    email : {
-        type : Sequelize.STRING,
-        allowNull : false,
-        unique: true
+    password:{
+        type : String,
+        required : true
     },
-
-    passWord: {
-        type : Sequelize.STRING,
-        allowNull :false
-        
-    },
-    
-    isPremiumUser : {
-        type : Sequelize.BOOLEAN,
-        defaultValue: false
+    isPremiumUser: {
+        type: Boolean,
+        default: false
     }
 });
 
-module.exports = Users;
+userSchema.virtual('orders', {
+    ref: 'Order',
+    localField: '_id',
+    foreignField: 'user'
+});
+
+module.exports = mongoose.model('User', userSchema)
+
